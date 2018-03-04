9a4b18cbb78c01d7eadb972fe1c171a95c00cab8
Kris with a K <kris.law.mac@gmail.com>
2018-03-04 14:31:29 -0600

    added new nodes:

--- SLAM_All/start_stop.cpp
+++ SLAM_All/start_stop.cpp
@@ -1,2 +1,35 @@
 #include <ros/ros.h>
 
+
+void Start(){
+
+}
+
+void Stop(){
+
+}
+
+void startStop(phoenix::start_stop::Request &req){
+	//start the SLAM algorithm running
+	if(*req == "start"){
+		Start();
+	}
+	else if(*req =="stop"){
+		Stop();
+	}
+	else{
+		// ??? invalid request
+	}
+}
+
+int main(int argc, char** argv){
+
+	ros::init(argc, argv, "start_stop");
+	ros::NodeHandle nh;
+
+	ros::ServiceServer service = nh.advertiseService("start_stop", startStop);
+
+	//need to read in the "start" or "stop" message and write those functions
+
+	return 0;
+}
