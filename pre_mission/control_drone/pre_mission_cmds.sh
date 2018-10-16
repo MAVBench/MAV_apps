6c9d925b56391e5676def23123075e0e0f33cd4f
zaddan <behzadboro@gmail.com>
2018-10-16 20:44:03 +0000

    control_drone premission added

---
+++ pre_mission/control_drone/pre_mission_cmds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sleep 4
+echo f 0 0 2 4
+sleep 4
+echo f -2 0 0 14
+sleep 7
+echo f 0 2 0 28
+sleep 14
+echo f 2 0 0 28
+sleep 14
+echo f 0 -2 0 28
+sleep 14
+echo f -2 0 0 14
+sleep 7
+echo t 0
+
