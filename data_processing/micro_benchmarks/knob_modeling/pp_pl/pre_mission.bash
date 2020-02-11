693b5dd7d400c6973e68d1e4e4b3b9fa4507ce8b
zaddan <behzadboro@gmail.com>
2020-02-11 13:09:47 -0500

    added pp_pl

---
+++ data_processing/micro_benchmarks/knob_modeling/pp_pl/pre_mission.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+cur_dir=$PWD 
+MAVApps_folder="../../../../../MAV_apps"
+cd ${MAVApps_folder}"/run_time/"
+python supervisor.py&
+cd $cur_dir
+sleep 10
+echo f 0 0 3 20 
+sleep 20
+#echo f 0 3 0 4
+#sleep 30
+echo f 0 0 0 10
+sleep 1
+echo c
+echo 30 480 30
+rosparam set knob_performance_modeling_for_piecewise_planner true
+sleep 3000
+echo kill > ${MAVApps_folder}"/data/package_delivery/supervisor_mailbox.txt"
+#${MAVApss_folder}isc/generic/kill_all_ros.bash
+
