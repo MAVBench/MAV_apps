8e69d3ae6cae844da35a7bfc12adbc2840c7ab8b
RadhikaG <radhikaghosal@gmail.com>
2020-02-12 18:47:43 -0500

    run_time: added dummy python node

---
+++ common/run_time/scripts/optimizer_node.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2.7
+
+from optimizer import bleh
+
+if __name__ == '__main__':
+    print "exec bleh"
+    bleh()
---
+++ common/run_time/src/optimizer/optimizer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+
+"""
+This file only contains the optimizer API which is called from 
+../scripts/optimizer_node.py
+"""
+
+def bleh():
+    print "bleh!"
