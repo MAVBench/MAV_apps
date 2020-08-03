464405d8d40e25d4b8c6cd72385885f8652de26d
RadhikaG <radhikaghosal@gmail.com>
2020-08-02 22:23:59 -0400

    plotting: zone analysis

---
+++ data_processing/micro_benchmarks/zone_analysis/plot_mission_metrics.py
@@ -0,0 +1,103 @@
+import matplotlib.pyplot as plt
+import json
+import sys
+import numpy as np
+import matplotlib.patches as patches
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+from mpl_toolkits.mplot3d import Axes3D 
+sys.path.append('../../common_utils')
+from data_parsing import *
+from EnvGenParser import EnvGenParser
+import os
+import seaborn as sns
+import argparse
+import pandas as pd
+
+col = sns.color_palette("Paired", 111111)
+stage_of_interests_to_pick_from = ["pc_om", "om_to_pl", "pp_pl", "pc_om_estimation"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--staticdata_path', type=str)
+parser.add_argument('-d', '--dynamicdata_path', type=str)
+parser.add_argument('-e', '--envfile_path', type=str)
+parser.add_argument('-r', '--resultdir_path', type=str)
+args = parser.parse_args()
+
+def dir_path(path):
+    if not os.path.isfile(path):
+        print("Couldn't find datafile!")
+        exit(-1)
+    else:
+        return os.path.dirname(os.path.realpath(path))
+
+# we dump resulting graphs in same dir as datafile
+
+input_filepath_static = args.staticdata_path
+input_filepath_dynamic = args.dynamicdata_path
+obs_stats_path = args.envfile_path
+if args.resultdir_path:
+    result_folder = args.resultdir_path
+else:
+    result_folder = dir_path(input_filepath_dynamic)
+
+metrics_to_collect_easy = [
+        "flight_time", 
+        "total_energy_consumed",
+        "distance_travelled"
+]
+
+metrics_to_collect_hard = []
+
+result_dict_static = parse_stat_file_flattened(input_filepath_static, metrics_to_collect_easy, metrics_to_collect_hard)
+result_dict_dynamic = parse_stat_file_flattened(input_filepath_dynamic, metrics_to_collect_easy, metrics_to_collect_hard)
+
+static_df = pd.DataFrame(result_dict_static)
+static_df["type"] = "Static"
+dynamic_df = pd.DataFrame(result_dict_dynamic)
+dynamic_df["type"] = "Dynamic"
+
+total_df = pd.concat([static_df, dynamic_df])
+
+fields = [
+        "flight_time", 
+        "total_energy_consumed",
+        "distance_travelled"
+]
+
+ylabels = [
+        "Flight time (s)",
+        "Total energy consumed (J)",
+        "Distance travelled (m)",
+]
+
+width = 0.35
+
+fig, axes = plt.subplots(1, len(fields))
+
+for i, field in enumerate(fields):
+    axes[i].bar(
+        [0 - width/2],
+        result_dict_dynamic[field],
+        width,
+        label="Dynamic"
+    )
+
+    axes[i].bar(
+        [0 + width/2],
+        result_dict_static[field],
+        width,
+        label="Static"
+    )
+
+    axes[i].set_title(field, fontsize=8)
+    axes[i].set_xlabel("")
+    axes[i].set_ylabel(ylabels[i])
+    axes[i].set_xticks([0])
+    axes[i].set_xticklabels(["Mission"])
+
+handles, labels = axes[len(fields)-1].get_legend_handles_labels()
+fig.legend(handles, labels, loc='upper right')
+fig.tight_layout()
+fig.savefig(result_folder + "/" + "overall_compare" + ".png")
+plt.close(fig)
